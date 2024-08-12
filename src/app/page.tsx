52453564ee3971f21a1c05c74a462c8e690fa267
Abhishek Panthee <80570587+abhishekpanthee@users.noreply.github.com>
2024-08-12 15:54:15 +0545

    Added clarity analyzer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Image from 'next/image'
 import { FiBook } from 'react-icons/fi'
 import {
@@ -12,7 +13,6 @@ import {
 } from 'react-icons/si'
 
 import Spotlight from '@/components/spotlight'
-
 import PrimaryLinks from './primary-links'
 
 const links = [
@@ -61,10 +61,25 @@ const links = [
     title: 'Dev.to',
     url: 'https://dev.to/abhishekpanthee'
   }
-  
 ]
 
 const HomePage = () => {
+  useEffect(() => {
+    const clarityScript = document.createElement('script')
+    clarityScript.type = 'text/javascript'
+    clarityScript.async = true
+    clarityScript.src = 'https://www.clarity.ms/tag/nlxuqpvtrj'
+
+    const scriptContent = `(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "nlxuqpvtrj");`
+
+    clarityScript.appendChild(document.createTextNode(scriptContent))
+    document.head.appendChild(clarityScript)
+  }, [])
+
   return (
     <>
       <div className='relative mx-auto flex flex-col items-center justify-center gap-4 pb-10 pt-24'>
@@ -91,7 +106,7 @@ const HomePage = () => {
               href={url}
               className='relative flex h-14 w-full items-center justify-center rounded-xl border border-zinc-700 bg-[#151414] px-8 py-4 transition-colors duration-300 hover:border-zinc-500'
               target='_blank'
-              rel='noopener, noreferrer'
+              rel='noopener noreferrer'
             >
               <div className='absolute left-8'>{icon}</div>
               <div className='text-zinc-100'>{title}</div>
